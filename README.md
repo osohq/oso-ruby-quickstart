2c044368f1b9425e552a8fdae16140027357617a
Gabe Jackson <gj@mail.co.de>
2020-10-05 18:26:52 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# oso Ruby Quickstart
+
+1. Clone this repository.
+2. To install dependencies: `bundle install`
+3. To run the server: `bundle exec ruby server.rb`
---
+++ server.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'oso'
+require 'webrick'
+
+require './expense'
+
+OSO ||= Oso.new
+OSO.load_file('expenses.polar')
+
+WEBrick::HTTPServer.new(Port: 5050).tap do |server|
+  server.mount_proc '/' do |req, res|
+    _, resource, id = req.path.split('/')
+    # Look up the requested expense in our "database"
+    expense = DB[id.to_i]
+
+    # 404 if the requested path doesn't match /expenses/:id
+    # or the requested expense ID doesn't exist in our "database"
+    if resource != 'expenses' || expense.nil?
+      res.status = 404
+      res.body = 'Not Found!'
+      next
+    end
+
+    actor = req.header['user']&.first
+    action = req.request_method
+
+    if OSO.allowed?(actor: actor, action: action, resource: expense)
+      res.body = expense.inspect
+    else
+      res.status = 403
+      res.body = 'Not Authorized!'
+    end
+  end
+  server.start
+end
